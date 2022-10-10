9ac8f6006db3cd7f3d5f4c0f3336c4858b2eac01
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-10-10 18:57:29 +0200

    chore: reuse Testimonial component using props

--- src/App.js
+++ src/App.js
@@ -8,7 +8,50 @@ function App() {
                 <h1>
                     Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:
                 </h1>
-                <Testimonial />
+                <Testimonial
+                    name="Shawn Wang"
+                    image=""
+                    country="Singapur"
+                    position="Ingeniero de Software"
+                    company="Amazon"
+                >
+                    Da miedo cambiar de carrera. Solo gané la confianza de que
+                    podía programar trabajando a través de los cientos de horas
+                    de lecciones gratuitas en freeCodeCamp. Tras pasar un año
+                    tuve un trabajo de seis cifras como ingeniero de software.{" "}
+                    <strong>freeCodeCamp cambió mi vida</strong>.
+                </Testimonial>
+                <Testimonial
+                    name="Sarah Chima"
+                    image=""
+                    country="Nigeria"
+                    position="Ingeniera de Software"
+                    company="ChatDesk"
+                >
+                    <strong>
+                        freeCodeCamp fue la puerta de entrada a mi carrera
+                    </strong>{" "}
+                    como desarrollador de software. El plan de estudios bien
+                    estructurado llevó mis conocimientos de programación de un
+                    nivel de principiante total a un nivel muy seguro. Era todo
+                    lo que necesitaba para conseguir mi primer trabajo de
+                    desarrollador en una empresa increíble.
+                </Testimonial>
+                <Testimonial
+                    name="Emma Bostian"
+                    image=""
+                    country="Suecia"
+                    position="Ingeniera de Software"
+                    company="Spotify"
+                >
+                    Siempre he tenido problemas para aprender JavaScript. He
+                    tomado muchos cursos, pero el curso de freeCodeCamp fue el
+                    que hizo clic. Estudiar JavaScript, así como estructuras de
+                    datos y algoritmos en{" "}
+                    <strong>freeCodeCamp me dió las habilidades</strong> y la
+                    confianza que necesitaba para conseguir el trabajo de mis
+                    sueños como ingeniera de software en Spotify.
+                </Testimonial>
             </div>
         </div>
     );
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,42 +1,36 @@
 import React from "react";
 import "./Testimonial.css";
 
-function Testimonial() {
+function Testimonial(props) {
+    const { name, country, position, company, children: quote } = props;
     return (
         <div className="testimonial">
             <div className="testimonial-header">
                 <img
                     className="testimonial-picture"
                     src={require("../images/testimonial-emma.png")}
-                    alt="Emma Bostian"
+                    alt={name}
                 />
             </div>
             <div className="testimonial-footer">
                 <div className="testimonial-meta">
                     <p className="testimonial-who">
-                        <span className="testimonial-who-name">
-                            Emma Bostian
-                        </span>{" "}
-                        en{" "}
-                        <span className="testimonial-who-location">Suecia</span>
+                        <span className="testimonial-who-name">{name}</span> en{" "}
+                        <span className="testimonial-who-location">
+                            {country}
+                        </span>
                     </p>
                     <p className="testimonial-job">
                         <span className="testimonial-job-position">
-                            Ingeniera de software
+                            {position}
                         </span>{" "}
                         en{" "}
-                        <span className="testimonial-job-company">Spotify</span>
+                        <span className="testimonial-job-company">
+                            {company}
+                        </span>
                     </p>
                 </div>
-                <div className="testimonial-quot">
-                    "Siempre he tenido problemas para aprender JavaScript. He
-                    tomado muchos cursos, pero el curso de freeCodeCamp fue el
-                    que se quedó. Estudiar JavaScript, así como estructuras de
-                    datos y algoritmos en{" "}
-                    <strong>freeCodeCamp me dio las habilidades</strong> y la
-                    confianza que necesitaba para conseguir el trabajo de mis
-                    sueños como ingeniero de software en Spotify."
-                </div>
+                <div className="testimonial-quot">"{quote}"</div>
             </div>
         </div>
     );
